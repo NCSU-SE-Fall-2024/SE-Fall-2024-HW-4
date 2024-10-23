a87690d6bb736021860ca6b1f1d54cb0cfe3aac6
Nikhilesh Cherukuri <44161680+nikki1234567@users.noreply.github.com>
2024-10-23 19:21:15 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Modify 2nd class passengers' gender labels to abbreviated form
+# Extract passengers from 2nd class who embarked at Southampton, replace male/female labels, and print them
 awk -F, 'BEGIN {OFS = FS} 
 NR == 1 {print; next} 
-$3 == 2 {
+$3 == 2 && $NF ~ /S/ {
     gsub(/female/, "F", $6)
     gsub(/male/, "M", $6)
-}
-{print}' titanic.csv > temp.csv && mv temp.csv titanic.csv
+    print
+}' titanic.csv
 
 # Calculate the average age of 2nd class passengers who embarked at Southampton
 awk -F, 'NR > 1 && $3 == 2 && $NF ~ /S/ {
-    gsub(/"/, "", $7)
-    gsub(/^[ \t]+|[ \t]+$/, "", $7)
+    gsub(/"/, "", $7)   # Clean up age field
+    gsub(/^[ \t]+|[ \t]+$/, "", $7)   # Trim whitespaces
 
-    if ($7 ~ /^[0-9]+(\.[0-9]+)?$/) {
+    if ($7 ~ /^[0-9]+(\.[0-9]+)?$/) {  # Ensure age is a valid number
         total_age += $7; passenger_count++
     }
 }
